13026bb306f3f6603065b0a3d3ead8f4b8d3330f
oNqNu <onanukintaro@gmail.com>
2022-01-28 17:23:20 +0900

    test

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1>こーくん</h1>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
