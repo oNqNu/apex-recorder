7c1bb0e778157b10a834124c6401df2831ecde13
yuma-code <onanukintaro@gmail.com>
2022-02-03 22:35:03 +0900

    ガチ頼む

--- src/App.js
+++ src/App.js
@@ -18,18 +18,16 @@ function App() {
       <chakra.main>
         <Container maxW='2xl'>
           <MyText>Apex Legends 戦績checker</MyText>
-          <FormControl as='fieldset'>
-            <FormLabel as='legend'>
-              プラットフォームを選択してください．
-            </FormLabel>
-            <RadioGroup defaultValue='Itachi'>
+          <FormControl>
+            <FormLabel>プラットフォームを選択してください．頼む．</FormLabel>
+            <RadioGroup defaultValue='psn'>
               <HStack spacing='24px'>
                 <Radio value='orogin'>origin</Radio>
                 <Radio value='psn'>play station</Radio>
                 <Radio value='xbl'>x box</Radio>
               </HStack>
             </RadioGroup>
-            <FormLabel htmlFor='first-name'>IDを入力してください．</FormLabel>
+            <FormLabel htmlFor='id'>IDを入力してください．</FormLabel>
             <Input id='id' placeholder='id' />
           </FormControl>
         </Container>
